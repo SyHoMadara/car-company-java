7d467d02442fe4db1f7de8235b2bbefb1a0496b4
SyHoMadara <sh.sarafrazi80@gmail.com>
2022-01-21 00:26:45 +0330

    DLlist initial

---
+++ src/DoubleLinkList.java
@@ -0,0 +1,28 @@
+class Node<T> {
+    public T data;
+    public Node<T> next;
+    public Node<T> pre;
+
+    Node(T data) {
+        next = null;
+        pre = null;
+    }
+    Node(){
+        next = null;
+        pre = null;
+    }
+}
+
+public class DoubleLinkList<T> {
+    Node<T> head, tail;
+    int size;
+    public DoubleLinkList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void insertAfter(Node<T> node ,T data){
+
+    }
+}
