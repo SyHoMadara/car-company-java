e18a9702d2dc17ab30da3a3ecf0cc8f3d2be3183
SyHoMadara <sh.sarafrazi80@gmail.com>
2022-01-21 16:32:07 +0330

    Car enum

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public enum Car {
+    SAMAND("this is samdn"),
+    PERAID("This is Peraid"),
+    PEGHO("This is Pegho");
+    String details;
+    Car(String details){
+        this.details = details;
+    }
+}
